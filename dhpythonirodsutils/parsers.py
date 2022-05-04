6d09b413fbbb5f2a3530bf4e4530e77bae2fef84
JonathanMELIUS <meliusjonathan@gmail.com>
2022-05-04 11:32:00 +0200

    Update parse_audit_trail_message [DHS-1842]

--- dhpythonirodsutils/parsers.py
+++ dhpythonirodsutils/parsers.py
@@ -2,26 +2,28 @@ import re
 from datetime import datetime
 
 AUDIT_TRAIL_REGEX = (
-    "^\[(?P<time_stamp>.*)\]\[AUDIT_TRAIL\]\[(?P<irods_user_id>\d*)\]\[(?P<topic>.*)\]\s-\s(?P<event>.*)$"
+    r"^\[(?P<time_stamp>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\]\[AUDIT_TRAIL\]"
+    r"(\[(?P<irods_user_id>\d*)\]|\[(?P<irods_user_name>\w*)\])\[(?P<topic>.*)\]\s-\s(?P<event>.*)$"
 )
 
 
 def parse_audit_trail_message(message, parse_time_stamp=False):
     """
+    Parameters
+    ----------
+    message: str
+        Audit log message
+    parse_time_stamp: bool
+        Whether to parse time_stamp to a python datetime object or not
 
-    Args:
-        message: str
-            Audit log message
-        parse_time_stamp: bool
-            Whether to parse time_stamp to a python datetime object or not
-
-    Returns:
-        Dictionary with the parsed audit log.
+    Returns
+    -------
+    dict
+        Parsed audit log with following keys:
             time_stamp
             irods_user_id
             topic
             event
-
     """
     re_match = re.match(AUDIT_TRAIL_REGEX, message)
     if re_match:
@@ -31,8 +33,9 @@ def parse_audit_trail_message(message, parse_time_stamp=False):
         else:
             output["time_stamp"] = re_match.group("time_stamp")
         output["irods_user_id"] = re_match.group("irods_user_id")
+        output["irods_user_name"] = re_match.group("irods_user_name")
         output["topic"] = re_match.group("topic")
         output["event"] = re_match.group("event")
         return output
 
-    raise Exception("No Match found. Unable to parse Audit log message")
+    raise ValueError("No Match found. Unable to parse Audit log message")
